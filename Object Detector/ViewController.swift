03545c554961f75056f92a51a059097cb2ec7363
rwapp <r.whitaker@mac.com>
2017-06-26 14:29:40 +0100

    Final tutorial

--- Object Detector/ViewController.swift
+++ Object Detector/ViewController.swift
@@ -15,12 +15,19 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
 	@IBOutlet private weak var cameraView: UIView?
 	@IBOutlet private weak var textLayer: UILabel?
 	
+	// set up the handler for the captured images
 	private let visionSequenceHandler = VNSequenceRequestHandler()
+	
+	// set up the camera preview layer
 	private lazy var cameraLayer: AVCaptureVideoPreviewLayer = AVCaptureVideoPreviewLayer(session: self.captureSession)
+	
+	// set up the capture session
 	private lazy var captureSession: AVCaptureSession = {
 		let session = AVCaptureSession()
 		session.sessionPreset = AVCaptureSession.Preset.photo
 		guard
+			
+			// set up the rear camera as the device to capture images from
 			let backCamera = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back),
 			let input = try? AVCaptureDeviceInput(device: backCamera)
 			else {
@@ -28,6 +35,7 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
 				return session
 				
 		}
+		// add the rear camera as the capture device
 		session.addInput(input)
 		return session
 	}()
@@ -36,12 +44,18 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
 		
+		// add the camera preview
 		self.cameraView?.layer.addSublayer(self.cameraLayer)
 		
+		// set up the delegate to handle the images to be fed to Core ML
 		let videoOutput = AVCaptureVideoDataOutput()
 		
+		// we want to process the image buffer and ML not on the main thread
 		videoOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "DispatchQueue"))
+		
 		self.captureSession.addOutput(videoOutput)
+		
+		// make the camera output fill the screen
 		cameraLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
 		
 		// begin the session
@@ -59,7 +73,71 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
 		// make sure the layer is the correct size
 		self.cameraLayer.frame = (self.cameraView?.frame)!
 	}
-
+	
+	// MARK: - Capture Session
+	
+	func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+		
+		// Get the pixel buffer from the capture session
+		guard let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
+		
+		// load the Core ML model
+		guard let visionModel:VNCoreMLModel = try? VNCoreMLModel(for: Inceptionv3().model) else { return }
+		
+		//  set up the classification request
+		let classificationRequest = VNCoreMLRequest(model: visionModel, completionHandler: handleClassification)
+		
+		// automatically resize the image from the pixel buffer to fit what the model needs
+		classificationRequest.imageCropAndScaleOption = VNImageCropAndScaleOptionCenterCrop
+		
+		// perform the machine learning classification
+		do {
+			try self.visionSequenceHandler.perform([classificationRequest], on: pixelBuffer)
+		} catch {
+			print("Throws: \(error)")
+		}
+	}
+	
+	// MARK - Core ML Classification
+	
+	func handleClassification(request: VNRequest, error: Error?) {
+		guard let observations = request.results else {
+			
+			// Nothing has been returned so we want to clear the label.
+			updateClassificationLabel(labelString: "")
+				
+			return
+		}
+		
+		let classifications = observations[0...3]
+			.flatMap({ $0 as? VNClassificationObservation}) // discard any erroneous results
+			.filter({ $0.confidence > 0.2 }) // discard anything with less than 20% accuracy.
+			.map(self.textForClassification) // get the text to display
+		
+			if (classifications.count > 0) {
+				// update the label to display what we found
+				updateClassificationLabel(labelString: "\(classifications.joined(separator: "\n"))")
+			} else {
+				// nothing matches our criteria, so clear the label
+				updateClassificationLabel(labelString: "")
+			}
+		
+	}
+	
+	func updateClassificationLabel(labelString: String) {
+		// We processed the capture session and Core ML off the main thread, so the compleetion handler was called onthe the same thread
+		// So we need to remember to get the main thread again tp update the UI
+		
+		DispatchQueue.main.async {
+			self.textLayer?.text = labelString
+		}
+	}
+	
+	func textForClassification(classification: VNClassificationObservation) -> String {
+		// Mapping the results from the VNClassificationObservation to a human readable string
+		let pc = Int(classification.confidence * 100)
+		return "\(classification.identifier)\nConfidence: \(pc)%"
+	}
+	
 
 }
-
